5d4daad580229d2794e0779947de007e81bcdbc0
Timothy Lewis <141955866+TimothyL123@users.noreply.github.com>
2023-11-01 10:29:20 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+// Create a class for vehicle. Each vehicle instance should have the following properties:
+
+// - make
+// - model
+// - year
+
+//Each vehicle instance should have access to a method called ***honk***, which returns the string “Beep.”
+class vehicle {
+    constructor(make, model, year) { 
+
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    // Each vehicle instance should have a method called toString, which returns the string containing the make, model and year.
+    toString() {
+        console.log('make, model, year');
+    }
+
+    display() {
+        console.log(`Vehicle with ${this.make} and ${this.model} and ${this.year}`);
+    }
+
+    honk() {
+        return "BEEP!";
+    }
+}
+
+
+// Create a class for a car. The Car class should inherit from Vehicle and each car instance should have a property called numWheels which has a value of 4.
+class car extends vehicle {
+    constructor (make, model, year) {
+        this.numWheels = 4;
+    }
+
+    carType() {
+        console.log('Vehicle is a 2010 Hundai 2010');
+    }
+}
+
+// Create a class for a Motorcycle. This class should inherit from Vehicle and each motorcycle instance should have a property called numWheels which has a value of 2. It should also have a revEngine method which returns “VROOM!!!”
+class motorcycle {
+    display() {
+        console.log('Vehicle is a loud & annoying Harley Davidson bike*');
+    }
+    numWheels() {
+        return 2;
+    }
+
+    revEngine() {
+        return "VROOM!";
+    }
+}
+
+
+// Create a class for a Garage. It should have a property called ***vehicles*** which will store an array of vehicles, and a property called ***capacity*** which is a number indicating how many vehicles will fit in the garage. When you create a garage, ***vehicles*** will always be empty; you only need to provide the ***capacity***.
+
+// A garage should also have an ***add*** method, which attempts to add a vehicle to the array of vehicles. However, if you try to add something which is *not* a vehicle, the garage should return the message “Only vehicles are allowed in here!”. Also, if the garage is at capacity, it should say “Sorry, we’re full.”
+class garage {
+    constructor(capacity) {
+        this.capacity = capacity
+        this.parked = 0;
+    }
+    parkCar(vehicle) {
+        if(this.parked === this.capacity){
+            throw new Error('Room must include no less than four vehicles!');
+        }
+        this.parked = this.parked + 1; 
+    }
+
+    greet() {
+        console.log('HELLO FROM THE GARAGE!!!');
+    }
+}
